dbd51e8137c3eaad70232c48346cefa0baf971c8
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-04-11 10:46:13 -0600

    promedio

---
+++ promedio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+float promedio(float a , float b)
+int main() {
+    float a=5, b=10, prom;
+    prom=promedio(a,b);
+    ṕrintf("el promedio es: %"2)
+    return 0;
+}
